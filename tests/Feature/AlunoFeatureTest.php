299e392d1b6f1133e7f96c1a51516c89f20c0e7d
Everton Muniz <munizeverton@gmail.com>
2018-02-08 17:54:57 -0200

    Removendo teste de paginacao

--- tests/Feature/AlunoFeatureTest.php
+++ tests/Feature/AlunoFeatureTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\AlunoBaseTest;
 
-class ExampleTest extends AlunoBaseTest
+class AlunoFeatureTest extends AlunoBaseTest
 {
     use DatabaseTransactions;
 
@@ -13,19 +13,6 @@ class ExampleTest extends AlunoBaseTest
     {
         $this->createFakeDataAluno(10);
         $response = $this->get('/alunos');
-
-        $response->assertStatus(200);
-        $page = new \DOMDocument();
-        $page->loadHTML($response->getContent());
-        /** @var \DOMElement[] $table */
-        $table = $page->getElementsByTagName('table');
-        $this->assertCount(11, $table[0]->getElementsByTagName('tr'));
-    }
-
-    public function testPaginateListAlunos()
-    {
-        $this->createFakeDataAluno(20);
-        $response = $this->get('/alunos?page=2');
 
         $response->assertStatus(200);
         $page = new \DOMDocument();
